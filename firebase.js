d2e91423f3130dd26a29802ec19e02839349f25e
nitin kushwaha <nitin348926@gmail.com>
2025-11-05 11:49:50 +0530

    5

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 // Initialize Firebase (replace with your config)
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAXHd6221PuMIgumYE-7NK-Pc_RqN6VB5A",
-    authDomain: "alumnihub-12193.firebaseapp.com",
-    projectId: "alumnihub-12193",
-    storageBucket: "alumnihub-12193.firebasestorage.app",
-    messagingSenderId: "238151366653",
-    appId: "1:238151366653:web:4c1ac1b98c83cf98610b94"
+    apiKey: "AIzaSyBSZhXbK-24REZvisLK_vs81dDG5Ivm-GE",
+  authDomain: "alumnihub-dea19.firebaseapp.com",
+  projectId: "alumnihub-dea19",
+  storageBucket: "alumnihub-dea19.firebasestorage.app",
+  messagingSenderId: "414163639977",
+  appId: "1:414163639977:web:c1c4759e5c61b377f153b8"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
